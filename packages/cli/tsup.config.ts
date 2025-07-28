5ff20a6dd89c386af8d71766b5fe4a352280a903
Charles Gibson <46542971+perasperaactual@users.noreply.github.com>
2025-07-28 08:48:25 -0400

    Switching all packages to use tsup only

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/cli.ts'],
   format: ['cjs'],
-  dts: false,
+  dts: true,
   splitting: false,
   sourcemap: false,
   clean: true,
