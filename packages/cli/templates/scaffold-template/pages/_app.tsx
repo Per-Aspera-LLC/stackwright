9920ebaaf982f65fe405aaf9a9fea67c4b67cfa3
Charles Gibson <46542971+perasperaactual@users.noreply.github.com>
2026-03-10 13:39:07 -0400

    Revert "fix: add shadcn registration to scaffold template + CI sync (#127)"
    This reverts commit e748ea25c102eca6414a1b4b0a48757bbb74d1bf.

--- packages/cli/templates/scaffold-template/pages/_app.tsx
+++ packages/cli/templates/scaffold-template/pages/_app.tsx
@@ -1,13 +1,10 @@
 import type { AppProps } from "next/app";
 import { registerDefaultIcons } from "@stackwright/icons";
 import { registerNextJSComponents } from "@stackwright/nextjs";
-import { registerShadcnComponents } from "@stackwright/ui-shadcn";
-import "@stackwright/ui-shadcn/styles.css";
 
-// Register Next.js adapter components (Image, Link, Router, Route), icons, and UI
+// Register Next.js adapter components (Image, Link, Router, Route) and icons
 registerNextJSComponents();
 registerDefaultIcons();
-registerShadcnComponents();
 
 export default function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
